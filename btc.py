d5b0f2f6050d41ea70a7f6e3f5ef8b02344d325a
KHS-SK <frog8930@naver.com>
2026-02-13 12:01:47 +0900

    py_05_파이썬_문제_추가

--- btc.py
+++ btc.py
@@ -1,5 +1,3 @@
-# 응용2
-
 import requests as req
 url = "https://api4.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 res = req.get(url).text
@@ -7,4 +5,7 @@ print(res)
 btc = res[29:34]
 print(str(int(btc) * 1450) + '원') # 1억 1389만 6050원
 won = str(int(btc) * 1450)
-print(won[:1] +'억 ' + won[1:5] + '만 ' + won[5:] + '원')
+
+print(won)
+# print(won[:1] +'억 ' + won[1:5] + '만 ' + won[5:] + '원')
+print(((won[-12:8] +'억 ') if won[-12:-8] else "") + won[-8:-4] + '만 ' + won[-4:] + '원')
